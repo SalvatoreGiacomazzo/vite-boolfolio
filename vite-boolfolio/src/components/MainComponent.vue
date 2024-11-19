fad8bcfd12e9998aff482ab70909f969e018b251
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-11-19 19:00:59 +0100

    displayed cards in maincomponent with bootstrap grid

--- vite-boolfolio/src/components/MainComponent.vue
+++ vite-boolfolio/src/components/MainComponent.vue
@@ -41,21 +41,21 @@ getWantedList(){
 </script>
 
 <template>
-<div class="container justify-content-center align-items-center">
-    <div class="row">
-        <div class="col-12">
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-</div>
-</div>
-</template>
+    <div class="container d-flex justify-content-center align-items-center flex-wrap">
+      <div class="row justify-content-center ">
+        <div class="col-4 mb-4" v-for="wanted in wantedList" :key="wanted.id">
+          <div class="card" style="width: 24rem;">
+            <img class="card-img-top" src="https://wallpapercave.com/wp/wp7819542.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">{{ wanted.name }} {{ wanted.last_name }}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
 
 
 <style lang="scss">
